1fd867dc7659d4dc2e94d220605d72c0a973c3a2
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-06-01 00:47:11 +0300

    Show block number

--- app/src/main/java/com/example/votekt/ui/box_contract/BoxViewModel.kt
+++ app/src/main/java/com/example/votekt/ui/box_contract/BoxViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.votekt.ui.box_contract
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.votekt.BuildConfig
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import org.web3j.protocol.Web3j
@@ -22,9 +24,10 @@ class BoxViewModel : ViewModel() {
         web3j = Web3j.build(HttpService(url))
     }
 
-    val state = MutableStateFlow<String>("0")
+    val balanceState = MutableStateFlow("0")
+    val netInfoState = MutableStateFlow("-")
 
-    fun load() {
+    fun loadBalance() {
         viewModelScope.launch(Dispatchers.IO) {
             val res = web3j.ethGetBalance(
                 BuildConfig.TEST_ADDRESS, DefaultBlockParameterName.LATEST
@@ -38,7 +41,20 @@ class BoxViewModel : ViewModel() {
 
             val formattedBalance = String.format("%.7f", roundedValue)
 
-            state.value = formattedBalance
+            balanceState.value = formattedBalance
+        }
+    }
+
+
+    fun loadNetInfo() {
+        viewModelScope.launch(Dispatchers.IO) {
+
+            while (true) {
+                val res = web3j.ethBlockNumber().send()
+                netInfoState.value = res.blockNumber.toString()
+
+                delay(5 * 1000)
+            }
         }
     }
 }
