30017a91169015c937def79ea286d7473dba7d98
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-05-31 20:59:33 +0300

    Init hardhat project

---
+++ _solidity/hardhat.config.js
@@ -0,0 +1,13 @@
+require('@nomiclabs/hardhat-ethers');
+require('dotenv').config()
+
+
+module.exports = {
+  solidity: "0.8.18",
+    networks: {
+     goerli: {
+       url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+       accounts: { mnemonic: process.env.MNEMONIC },
+     },
+   },
+};
