2d192e1bb75ab18a882eb9f270753db87a8c286e
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-06-14 23:16:42 +0300

    ui

--- app/src/main/java/com/example/votekt/ui/voting_details/VotingDetailsScreen.kt
+++ app/src/main/java/com/example/votekt/ui/voting_details/VotingDetailsScreen.kt
@@ -8,7 +8,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -16,6 +20,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.votekt.ui.components.VotersPager
 import com.example.votekt.ui.components.VotingBar
@@ -32,7 +37,15 @@ fun VotingDetailsScreen() {
                 title = {
                     Text(text = "Voting res", style = MaterialTheme.typography.headlineLarge)
                 },
-                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = MaterialTheme.colorScheme.background),
+                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Transparent),
+                navigationIcon = {
+                    IconButton(onClick = { }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                }
             )
         }) { pv ->
             Column(Modifier.padding(start = 16.dp, end = 16.dp, top = pv.calculateTopPadding() + 16.dp)) {
