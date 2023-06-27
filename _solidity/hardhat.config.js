aa69e085637ef0486bb55cf07eae303bf228c1ef
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-06-27 23:20:39 +0300

    Set mining time to 3 sec for local node

--- _solidity/hardhat.config.js
+++ _solidity/hardhat.config.js
@@ -9,5 +9,9 @@ module.exports = {
        url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
        accounts: { mnemonic: process.env.MNEMONIC },
      },
+
+    hardhat: {
+      blockTime: 3000,
+    },
    },
 };
