fe58356c3578da91c277988f08551a272854a36d
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-07-26 22:38:02 +0300

    Fix

--- app/src/main/java/com/example/votekt/ui/components/ProposalCard.kt
+++ app/src/main/java/com/example/votekt/ui/components/ProposalCard.kt
@@ -1,13 +1,11 @@
 package com.example.votekt.ui.components
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -62,19 +60,19 @@ fun ProposalCard(
                 Spacer(modifier = Modifier.height(12.dp))
             }
 
-            Box(
-                modifier = Modifier
-                    .width(100.dp)
-                    .wrapContentHeight(),
-            ) {
-                VotingBarCircle(
-                    params = VotingBarParams(
-                        votesFor = proposal.votesFor,
-                        votesAgainst = proposal.votesAgainst,
-                        segmentWidth = 12.dp
-                    )
-                )
-            }
+//            Box(
+//                modifier = Modifier
+//                    .width(100.dp)
+//                    .wrapContentHeight(),
+//            ) {
+//                VotingBarCircle(
+//                    params = VotingBarParams(
+//                        votesFor = proposal.votesFor,
+//                        votesAgainst = proposal.votesAgainst,
+//                        segmentWidth = 12.dp
+//                    )
+//                )
+//            }
 
         }
 
