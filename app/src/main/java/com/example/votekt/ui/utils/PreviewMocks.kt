cfe70fdf67023b7afb4df258bcfb21aed9abf229
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2024-02-04 15:50:11 +0300

    Add PreviewMocks.kt

---
+++ app/src/main/java/com/example/votekt/ui/utils/PreviewMocks.kt
@@ -0,0 +1,14 @@
+package com.example.votekt.ui.utils
+
+import com.example.votekt.domain.votings.Proposal
+import com.example.votekt.domain.votings.VotingData
+
+fun Proposal.Companion.mock(): Proposal {
+    return Proposal(
+        id = 0,
+        title = "My awesome Proposal",
+        description = "My awesome description. Lorem ipsum lorem ipsum. My awesome description. Lorem ipsum lorem ipsum.",
+        votingData = VotingData(10, 25),
+        expirationTime = 0
+    )
+}
