549fe03e53d0b27d96d949ebe50c295f647928c9
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2024-03-20 22:58:06 +0300

    Update insets handling

--- app/src/main/java/com/example/votekt/MainActivity.kt
+++ app/src/main/java/com/example/votekt/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.votekt.ui.core.AppNavHost
 import com.example.votekt.ui.theme.VoteKtTheme
@@ -16,12 +18,20 @@ import com.example.votekt.ui.theme.VoteKtTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             val navController = rememberNavController()
 
             VoteKtTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier
+                        .safeDrawingPadding()
+                        .fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
                     AppNavHost(navController = navController)
                 }
             }
