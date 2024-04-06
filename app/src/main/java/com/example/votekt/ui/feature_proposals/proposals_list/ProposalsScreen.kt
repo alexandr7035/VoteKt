377334c103b1747fc356f44d5fc96240e41d85b1
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2024-04-06 23:49:32 +0300

    FAB design update

--- app/src/main/java/com/example/votekt/ui/feature_proposals/proposals_list/ProposalsScreen.kt
+++ app/src/main/java/com/example/votekt/ui/feature_proposals/proposals_list/ProposalsScreen.kt
@@ -17,15 +17,18 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -55,16 +58,16 @@ fun ProposalsScreen(
             AppBar(title = stringResource(R.string.proposals))
         },
         floatingActionButton = {
-            ExtendedFloatingActionButton(
-                onClick = { onNewProposalClick.invoke() },
-                icon = {
-                    Icon(
-                        Icons.Outlined.Add,
-                        contentDescription = stringResource(R.string.add)
-                    )
-                },
-                text = { Text(stringResource(R.string.create)) }
-            )
+            FloatingActionButton(
+                onClick = onNewProposalClick,
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary,
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Add,
+                    contentDescription = stringResource(id = R.string.create_proposal)
+                )
+            }
         }) { pv ->
 
         val state = viewModel.state.collectAsStateWithLifecycle().value
