98cc53b9dbc543965dbba735726fc391de892db8
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2024-04-16 23:56:20 +0300

    Fix error messages

--- app/src/main/java/com/example/votekt/ui/Errors.kt
+++ app/src/main/java/com/example/votekt/ui/Errors.kt
@@ -8,9 +8,9 @@ fun ErrorType.asTextError(): UiText {
     return when (this) {
         ErrorType.NODE_CONNECTION_ERROR -> UiText.StringResource(R.string.no_connection)
         ErrorType.MNEMONIC_CONFIRMATION_INCOMPLETE -> UiText.StringResource(R.string.error_mnemonic_confirmation_incomplete)
-        ErrorType.MNEMONIC_CONFIRMATION_WRONG -> UiText.StringResource(R.string.error_mnemonic_incomplete)
+        ErrorType.MNEMONIC_CONFIRMATION_WRONG -> UiText.StringResource(R.string.error_mnemonic_confirmation_invalid)
         ErrorType.MNEMONIC_INVALID -> UiText.StringResource(R.string.error_mnemonic_invalid)
-        ErrorType.MNEMONIC_INVALID_WORD_COUNT -> UiText.StringResource(R.string.error_mnemonic_confirmation_invalid)
+        ErrorType.MNEMONIC_INVALID_WORD_COUNT -> UiText.StringResource(R.string.error_mnemonic_incomplete)
 
         ErrorType.UNKNOWN_ERROR -> UiText.StringResource(R.string.unknown_error)
     }
