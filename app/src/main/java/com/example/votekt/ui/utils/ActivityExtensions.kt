74a2b95d6df2b9e354053f8cb8df9061c664d5d0
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2024-04-28 20:42:58 +0300

    Add screenshot lock extensions

---
+++ app/src/main/java/com/example/votekt/ui/utils/ActivityExtensions.kt
@@ -0,0 +1,14 @@
+package com.example.votekt.ui.utils
+
+import android.app.Activity
+import android.view.WindowManager
+
+fun Activity.lockScreenshots() {
+    window.setFlags(
+        WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE
+    )
+}
+
+fun Activity.unlockScreenshots() {
+    window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
+}
